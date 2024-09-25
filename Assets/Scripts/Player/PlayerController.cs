b121eaa533b881e556152fbba117c69a403bf885
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-09-25 20:12:43 +0700

    animation player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
-    private Vector2 movement;
+
     private Rigidbody2D rd;
 
     private Vector3 direction;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator myAnimator;
 
     private void Awake()
     {
 
         rd = this.GetComponent<Rigidbody2D>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        myAnimator = this.GetComponent<Animator>();
     }
 
 
@@ -24,17 +29,40 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void Move()
     {
-        this.transform.position += direction * moveSpeed * Time.deltaTime;
+        if (direction != Vector3.zero)
+        {
+            this.transform.position += direction * moveSpeed * Time.deltaTime;
+            myAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isWalking", false);
+        }
     }
 
     private Vector3 MoveDirection(float _moveHorizontal, float _moveVertical)
     {
         return new Vector3(_moveHorizontal, _moveVertical, 0);
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        if (mousePos.x > playerScreenPoint.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (mousePos.x < playerScreenPoint.x)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 
 }
